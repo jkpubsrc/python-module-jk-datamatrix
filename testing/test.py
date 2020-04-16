8151b62141e3fc8efcde57a1b8d664e01f4111ac
jkpubsrc <pubsrc@binary-overflow.de>
2020-04-16 12:30:54 +0200

    Initial release.

---
+++ testing/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+
+
+
+from jk_datamatrix import DataMatrix
+
+
+
+
+
+
+m = DataMatrix([ "word", "number", "status" ])
+m.addRow("Apple", 123, True)
+m.addRow("Carrot", 234, False)
+m.addRow("Pear", 345, True)
+m.addRow("Banana", 456, False)
+m.addRow("Cherry", 567, True)
+m.addRow("Blackberry", 678, False)
+m.addRow("Avocado", 789, True)
+
+m.dump()
+
+m.orderByColumn("word")
+
+m.dump()
+
+m.orderByColumn("number")
+
+m.dump()
+
+m.orderByColumn("status")
+
+m.dump()
+
+
+
+
